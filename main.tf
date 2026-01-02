efdb3f0843be891d09df38f3e5cfa954162a3028
root <root@ip-172-31-90-244.ec2.internal>
2026-01-02 10:20:24 +0000

    Correct main.tf for us-east-1 with dynamic AZs

--- main.tf
+++ main.tf
@@ -1,23 +1,11 @@
-module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "~> 20.0"
+############################
+# Fetch Availability Zones
+############################
+data "aws_availability_zones" "available" {}
 
-  cluster_name    = var.cluster_name
-  cluster_version = "1.29"
-
-  vpc_id          = module.vpc.vpc_id
-  subnet_ids      = module.vpc.private_subnets
-
-  eks_managed_node_groups = {
-    default = {
-      desired_size = 2
-      max_size     = 2
-      min_size     = 1
-      instance_types = ["t3.medium"]
-    }
-  }
-}
-
+############################
+# VPC
+############################
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.0"
@@ -25,10 +13,45 @@ module "vpc" {
   name = "eks-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["ap-south-1a", "ap-south-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  azs = slice(data.aws_availability_zones.available.names, 0, 2)
+
+  private_subnets = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+
+  public_subnets = [
+    "10.0.101.0/24",
+    "10.0.102.0/24"
+  ]
 
   enable_nat_gateway = true
+  single_nat_gateway = true
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
+############################
+# EKS Cluster
+############################
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size   = 1
+      min_size       = 1
+      max_size       = 1
+      instance_types = ["t3.medium"]
+    }
+  }
 }
 
